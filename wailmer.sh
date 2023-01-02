4ad9abc43441294e6ec4775c4ef37fde872b699b
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2023-01-01 22:37:35 -0500

    Update and rename wailmer-ubuntu.sh to wailmer.sh

---
+++ wailmer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/TraceEntertains/wailmer/main/spheal-core.sh
+chmod +x ./spheal-core.sh
+sudo ./spheal-core.sh
+
+git clone https://github.com/fortheusers/hb-appstore --recursive
+cd hb-appstore
+make $PLATFORM
+
+echo Build attempted, if failed, please try to resolve errors and run "make $PLATFORM" again.
