dff6deded90207a1af809eea4327a36e4c3327bb
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2023-01-01 23:27:54 -0500

    Update wailmer.sh

--- wailmer.sh
+++ wailmer.sh
@@ -2,7 +2,7 @@
 
 wget https://raw.githubusercontent.com/TraceEntertains/wailmer/main/spheal-core.sh
 chmod +x ./spheal-core.sh
-sudo ./spheal-core.sh
+./spheal-core.sh
 
 git clone https://github.com/fortheusers/hb-appstore --recursive
 cd hb-appstore
